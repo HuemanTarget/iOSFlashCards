61d043f3b5876330cd042248ec2e81acf7b5b8eb
Joshua Basche <pompousair@Joshuas-MacBook-Air.local>
2022-02-04 10:07:06 -0800

    initial commit

---
+++ iOSFlashCard/iOSFlashCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSFlashCardApp.swift
+//  iOSFlashCard
+//
+//  Created by Joshua Basche on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSFlashCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
